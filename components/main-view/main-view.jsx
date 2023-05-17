678dd5bb0fb61de4a635b4dfab188e4462aacfe7
fouad <f.ahsayni@pm.me>
2023-05-17 21:23:26 +0200

    Update main-view

--- components/main-view/main-view.jsx
+++ components/main-view/main-view.jsx
@@ -1,30 +1,28 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import { MovieCard } from "../movie-card/movie-card";
 import { MovieView } from "../movie-view/movie-view";
 
 export const MainView = () => {
-  const [movies, setMovies] = useState([
-    {
-      id: 1,
-      title: "The Last Samurai",
-      director: "Edward Zwick",
-      genre: "Historical"
-    },
-    {
-      id: 2,
-      title: "Interstellar",
-      director: "Chrisopher Nolan",
-      genre: "Science Fiction"
-    },
-    {
-      id: 3,
-      title: "Spirited Away",
-      director: "Hayao Miyazaki",
-      genre: "Anime"
-    },
-  ]);
+  const [movies, setMovies] = useState([]);
 
   const [selectedMovie, setSelectedMovie] = useState(null);
+
+  useEffect(() => {
+    fetch("https://movie-api-xy68.onrender.com/movies")
+      .then((response) => response.json())
+      .then((data) => {
+        const moviesFromApi = data.map(movie => {
+          return {
+            id: movie._id,
+            title: movie.Title,
+            director: movie.Director.Name,
+            genre: movie.Genre.Name
+          };
+        });
+
+        setMovies(moviesFromApi);
+      });
+  }, []);
 
   if (selectedMovie) {
     return (
@@ -37,17 +35,17 @@ export const MainView = () => {
   }
 
   return (
-      <div>
-        {movies.map((movie) => (
-            <MovieCard
-                key={movie.id}
-                movie={movie}
-                onMovieClick={(newSelectedMovie) => {
-                  setSelectedMovie(newSelectedMovie);
-                }}
-            />
-        ))}
-      </div>
+    <div>
+      {movies.map((movie) => (
+        <MovieCard
+          key={movie.id}
+          movie={movie}
+          onMovieClick={(newSelectedMovie) => {
+            setSelectedMovie(newSelectedMovie);
+          }}
+        />
+      ))}
+    </div>
   );
 
 };
