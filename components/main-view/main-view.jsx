70c66d3711dfdc6f28b2064d4d5b6c89e39a4435
fouad <f.ahsayni@pm.me>
2023-05-20 14:05:32 +0200

    Update main-view

--- components/main-view/main-view.jsx
+++ components/main-view/main-view.jsx
@@ -2,6 +2,7 @@ import { useState, useEffect } from "react";
 import { MovieCard } from "../movie-card/movie-card";
 import { MovieView } from "../movie-view/movie-view";
 import { LoginView } from "../login-view/login-view"
+import { SignupView } from "../signup-view/signup-view"
 
 export const LoginView = ({ onLoggedIn }) => {
   const [movies, setMovies] = useState([]);
@@ -27,12 +28,14 @@ export const LoginView = ({ onLoggedIn }) => {
 
   if (!user) {
     return (
-      <LoginView
-        onLoggedIn={(user, token) => {
+      <div>
+        <LoginView onLoggedIn={(user, token) => {
           setUser(user);
           setToken(token);
-        }}
-      />
+        }} />
+        or
+        <SignupView />
+      </div>
     );
   }
 
