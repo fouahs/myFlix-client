ddf87edd8eb7815bf1099445c46f78f2e8871b5a
fouad <f.ahsayni@pm.me>
2023-05-20 14:23:58 +0200

    Update login-view

--- components/login-view/login-view.jsx
+++ components/login-view/login-view.jsx
@@ -19,21 +19,20 @@ export const LoginView = () => {
       },
       body: JSON.stringify(data)
     })
-      .then((response) => response.json())
-      .then((data) => {
-        console.log("Login response: ", data);
-        if (data.user) {
-          localStorage.setItem("user", JSON.stringify(data.user));
-          localStorage.setItem("token", data.token);
-          onLoggedIn(data.user, data.token);
-        } else {
-          alert("No such user");
-        }
-      })
-      .catch((e) => {
-        alert("Something went wrong");
-      });
-
+        .then((response) => response.json())
+        .then((data) => {
+          console.log("Login response: ", data);
+          if (data.user) {
+            localStorage.setItem("user", JSON.stringify(data.user));
+            localStorage.setItem("token", data.token);
+            onLoggedIn(data.user, data.token);
+          } else {
+            alert("No such user");
+          }
+        })
+        .catch((e) => {
+          alert("Something went wrong");
+        });
   };
 
   return (
